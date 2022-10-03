8f992b6d20770885eb64464aa472a6bb55c3abad
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2022-10-03 16:32:47 +0200

    Header added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
-function Header() {
+export default function Header() {
     return (
         <header>
-            <img src="../images/meme.png" ></img>
+            <img src="../images/meme.png" alt="Meme"></img>
             <h2>Meme Generator</h2>
             <h4>React Course - Project 3</h4>
         </header>
