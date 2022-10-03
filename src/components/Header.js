6a68a2c58f1120c88082e75b9ac8a4c77e5ad6c1
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2022-10-03 17:07:07 +0200

    Style file added for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
+import "../style/header.css"
+
 export default function Header() {
     return (
-        <header>
-            <img src="../images/meme.png" alt="Meme"></img>
+        <header className="header">
+            <img src="../images/meme.png" alt="Meme"/>
             <h2>Meme Generator</h2>
             <h4>React Course - Project 3</h4>
         </header>
