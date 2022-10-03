54045e9ba1e8cfab41157f68200bbd8be9649dc1
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2022-10-03 15:10:41 +0200

    Project Added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Shit!!!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header>
+            <img src="../images/meme.png" ></img>
+            <h2>Meme Generator</h2>
+            <h4>React Course - Project 3</h4>
+        </header>
+    )
+}
