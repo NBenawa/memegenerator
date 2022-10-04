fdf394e929e1a685e87b05e338af53d6b0c8aadd
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2022-10-04 13:55:20 +0200

    New functionality added

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,4 +1,15 @@
+import React from 'react';
+import image_data from '../memesData.js';
+
 export default function Content() {
+    let [resultImage, setResultImage] = React.useState("")
+    function getRandomUrl() {
+        const data = image_data.data.memes;
+        const rand_item = Math.floor(Math.random()*data.length);
+        const url = data[rand_item]["url"];
+        
+        setResultImage(url)
+    }
     return (
         <div className="content">
             <div className="form">
@@ -6,9 +17,9 @@ export default function Content() {
                     <input className="form-input" placeholder="Top Text"/>
                     <input className="form-input" placeholder="Bottom Text"/>
                 </div>
-                <button className="form-button">Get a new meme image</button>
+                <button className="form-button" onClick={getRandomUrl}>Get a new meme image</button>
             </div>
-            <img className="result-image" alt="Result" />
+            <img src={resultImage} className="result-image" alt="Result" />
         </div>
     )
 }
